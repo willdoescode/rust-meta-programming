d9e8f547c4b062aee679eec0c1bea2fa426e0f4b
Will Lane <williamlane923@gmail.com>
2021-03-24 17:00:48 -0700

    added build fn

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@ macro_rules! a_macro {
 	)
 }
 
+// expr is a designator type, $e can be any expression
 macro_rules! x_and_y {
 	(x => $e:expr) => (println!("X: {}", $e));
 	(y => $e:expr) => (println!("Y: {}", $e));
+}
+
+// ident is an identifier which is typically a function name or var name
+macro_rules! build_fn {
+	($func_name:ident) => (
+		fn $func_name() {
+			println!("Called {:#?}()", stringify!($func_name));
+		}
+	)
 }
 
 fn main() {
 	a_macro!();
 	x_and_y!(x => 10);
 	x_and_y!(y => 10 + 20);
+	build_fn!(hello);
+	hello();
 }
